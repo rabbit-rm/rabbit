444c7a738a5a99e215025af2845664aa65d6d534
rabbit <rabbit.rm99@gmail.com>
2024-03-22 11:19:01 +0800

    jwtKit

--- jwtKit/jwt.go
+++ jwtKit/jwt.go
@@ -2,31 +2,26 @@ package jwtKit
 
 import (
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/rabbit-rm/rabbit/errorKit"
 )
 
-type JWT struct {
-	key []byte
+func NewWithClaims(secretKey interface{}, signingMethod jwt.SigningMethod, claims jwt.Claims, opts ...jwt.TokenOption) (string, error) {
+	return jwt.NewWithClaims(signingMethod, claims, opts...).SignedString(secretKey)
 }
 
-func NewJWT(key []byte) *JWT {
-	return &JWT{key: key}
-}
-
-func (j *JWT) NewWithClaims(signingMethod jwt.SigningMethod, claims jwt.Claims, opts ...jwt.TokenOption) (string, error) {
-	return jwt.NewWithClaims(signingMethod, claims, opts...).SignedString(j.key)
-}
-
-func (j *JWT) Parse(tokenString string, keyFunc jwt.Keyfunc, opts ...jwt.ParserOption) (jwt.MapClaims, error) {
-	token, err := jwt.NewParser(opts...).Parse(tokenString, keyFunc)
-	if err != nil {
-		return nil, err
-	}
-	if claims, ok := token.Claims.(jwt.MapClaims); ok {
-		return claims, nil
-	}
-	return nil, nil
-}
-func (j *JWT) ParseWithClaims(tokenString string, claims jwt.Claims, keyFunc jwt.Keyfunc, opts ...jwt.ParserOption) (*jwt.Token, error) {
+func ParseWithClaims[T jwt.Claims](tokenString string, claims T, keyFunc jwt.Keyfunc, opts ...jwt.ParserOption) (value T, err error) {
 	token, err := jwt.NewParser(opts...).ParseWithClaims(tokenString, claims, keyFunc)
-	return token, err
+	if err != nil {
+		return
+	}
+	if !token.Valid {
+		err = errorKit.New("invalid token")
+		return
+	}
+	var ok bool
+	if value, ok = token.Claims.(T); !ok {
+		err = errorKit.New("invalid claims type(%T)", token.Claims)
+		return
+	}
+	return
 }
--- jwtKit/jwt_test.go
+++ jwtKit/jwt_test.go
@@ -3,8 +3,6 @@ package jwtKit
 import (
 	"fmt"
 	"log"
-	"os"
-	"path"
 	"testing"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -16,33 +14,13 @@ func TestNewJWT(t *testing.T) {
 		Issuer: "RM",
 		ID:     "1",
 	}
-	j := NewJWT(signKey)
-	text, err := j.NewWithClaims(jwt.SigningMethodHS256, claims)
+	text, err := NewWithClaims(signKey, jwt.SigningMethodHS256, claims)
 	fmt.Println(text, err)
-	token, err := j.ParseWithClaims(text, &jwt.RegisteredClaims{}, func(token *jwt.Token) (interface{}, error) {
-		return j.key, nil
+	v, err := ParseWithClaims(text, &jwt.RegisteredClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return signKey, nil
 	})
 	if err != nil {
 		log.Fatal(err)
 	}
-	if v, ok := token.Claims.(*jwt.RegisteredClaims); ok {
-		fmt.Println(v.Issuer, v.ID)
-	}
-}
-
-func TestPathJoin(t *testing.T) {
-	root := "W:\\A\\B\\C\\D\\E"
-	join := path.Join(root, "..\\..\\DD")
-	err := os.Chdir(join)
-	if err != nil {
-		log.Fatal(err)
-	}
-	getwd, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(join)
-	fmt.Println(getwd)
-
+	fmt.Println(v.Issuer, v.ID)
 }
